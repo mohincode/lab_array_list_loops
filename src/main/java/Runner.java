f2d660bf982bdbdae5e0d947841cc527df509a10
Mohamed <mohamed@Mohameds-MacBook-Pro.local>
2023-01-23 16:15:59 +0000

    completed first island section

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -15,14 +15,45 @@ public class Runner {
         scottishIslands.add("Tresco");
 
 //        1. Add "Coll" to the end of the list
+
+        scottishIslands.add("Coll");
+
 //        2. Add "Tiree" to the start of the list
+
+        scottishIslands.add(0, "Tiree");
+
 //        3. Add "Islay" after "Jura" and before "Mull"
+
+        scottishIslands.add(scottishIslands.indexOf("Jura")+1, "Islay");
+
 //        4. Print out the index position of "Skye"
+
+        System.out.println(scottishIslands.indexOf("Skye"));
+
 //        5. Remove "Tresco" from the list by name
+
+        scottishIslands.remove("Tresco");
+
 //        6. Remove "Arran" from the list by index
+
+        scottishIslands.remove(scottishIslands.indexOf("Arran"));
+
+
 //        7. Print the number of islands in your arraylist
+
+        System.out.println(scottishIslands.size());
+
 //        8. Sort the list alphabetically
+
+        Collections.sort(scottishIslands);
+
 //        9. Print out all the islands using a for loop
+
+        for (String scottishIsland : scottishIslands) {
+            System.out.print(scottishIsland + " ");
+        }
+
+
 
         System.out.println(scottishIslands);
 
@@ -33,14 +64,28 @@ public class Runner {
         System.out.println("numbers: " + numbers); // requires loops
 
 //        1. Print out a list of the even integers
+
+
+
 //        2. Print the difference between the largest and smallest value
+
+
+
 //        3. Print True if the list contains a 1 next to a 1 somewhere.
+
+
+
 //        4. Print the sum of the numbers,
+
+
+
 //        5. Print the sum of the numbers...
 //           ...except the number 13 is unlucky, so it does not count...
 //           ...and numbers that come immediately after a 13 also do not count.
 //
 //          So [2, 7, 13, 2] would have sum of 9.
+
+
 
     }
 
